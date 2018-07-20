210c5daf609b448817985a9a132d0f74274f5402
malempatiu <malempatiu@gmail.com>
2018-07-20 19:19:25 +0200

    updating DB

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const imageUrl   = require('./routes/imageurl');
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
